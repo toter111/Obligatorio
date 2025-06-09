4d1f7f4189538fa5361f3e8fd30be116cbfe92fd
Feerico <fedesone06@gmail.com>
2025-06-09 00:40:29 -0300

    s

--- requerimiento.py
+++ requerimiento.py
@@ -1,8 +1,41 @@
+from pieza import Pieza
 
+class Reposicion:
+    def __init__(self, pieza, cantidad, fecha):
+        self.__pieza = pieza
+        self.__cantidad = cantidad
+        self.__fecha = fecha
 
-class requerimiento:
-    def __init__(self,maquina,pieza,cantidad):
-        self.maquina=maquina
-        self.pieza=pieza
-        self.cantidad=cantidad
+    @property
+    def pieza(self):
+        return self.__pieza
     
+    @pieza.setter
+    def pieza(self, value):
+        if not isinstance(value, Pieza):
+            raise ValueError("Debe proporcionar un objeto Pieza válido")
+        self.__pieza = value
+        
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    
+    @cantidad.setter
+    def cantidad(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("La cantidad debe ser un entero positivo")
+        self.__cantidad = value
+        
+    @property
+    def fecha(self):
+        return self.__fecha
+    
+    @fecha.setter
+    def fecha(self, value):
+        self.__fecha = value
+
+    def costo(self):
+        return self.__cantidad * self.__pieza.costo_unitario()
+
+    def __str__(self):
+        return f"Reposición: Pieza {self.__pieza.descPieza}, Cantidad: {self.__cantidad}, Fecha: {self.__fecha}, Costo: ${self.costo():.2f}"
